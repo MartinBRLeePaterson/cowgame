3125ec843a33626c37e424840d9ff36b48112c88
Martin <awildmartinappeared@gmail.com>
2018-01-17 09:31:33 +0800

    I did some chaaaaaaaaaaaaanges

--- map generation.py
+++ map generation.py
@@ -1,12 +1,12 @@
 import sys
 
 map = []
-for alphaiter in range(20):
+for alphaiter in range(40):
     map.append([])
-    for betaiter in range(20):
+    for betaiter in range(40):
         map[alphaiter].append("#")
 fog = map
-for alphaiter in range(20):
-    for betaiter in range(20):
-        sys.stdout.write(map[alphaiter][betaiter] + "  ")
+for alphaiter in range(40):
+    for betaiter in range(40):
+            sys.stdout.write(map[alphaiter][betaiter] + "  ")
     print("")
